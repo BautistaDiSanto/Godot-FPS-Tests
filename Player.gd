0e3840670c2545727885b5f2e100078bda476811
Bautista <juanbautistadisanto@gmail.com>
2022-09-21 22:45:49 -0300

    add barrel, add double jump & dash

--- Player.gd
+++ Player.gd
@@ -1,9 +1,18 @@
 extends KinematicBody
 
-var speed = 7
+var base_speed = 7
+var current_speed = 7
 var acceleration = 20
 var gravity = 9.8
+
 var jump = 5
+var available_jumps = 2
+var jumped = 0
+
+var dash_speed = 3
+var dash_duration = 0.3
+var dash_timer = null
+var can_dash = true
 
 var mouse_sensivility = 0.05
 
@@ -13,8 +22,21 @@ var fall = Vector3()
 
 onready var head = $Head
 
+func dash_timer():
+	print("dash_timer")
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	dash_timer = Timer.new()
+	dash_timer.set_one_shot(true)
+	dash_timer.set_wait_time(dash_duration)
+	dash_timer.connect("timeout", self, "dash_finished")
+	add_child(dash_timer)
+	
+func dash_finished():
+	can_dash = true
+	current_speed = base_speed
+	print("DASH READY!")
 	
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -26,16 +48,10 @@ func _process(delta):
 	
 	direction = Vector3()
 	
-	if not is_on_floor():
-		fall.y -= gravity * delta
-	
-	if Input.is_action_just_pressed("jump"):
-		fall.y = jump
-	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-	if Input.is_action_pressed("move_forward"): 
+	if Input.is_action_pressed("move_forward"):
 		direction -= transform.basis.z
 
 	elif Input.is_action_pressed("move_backward"):
@@ -47,7 +63,22 @@ func _process(delta):
 	elif Input.is_action_pressed("move_right"):
 		direction += transform.basis.x
 		
+	if is_on_floor():
+		jumped = 0
+	else: 
+		fall.y -= gravity * delta
+
+	if (Input.is_action_just_pressed("jump")) and (jumped < available_jumps):
+		jumped = jumped + 1
+		fall.y = jump
+	
+	if (Input.is_action_just_pressed("enable_dash") && can_dash):
+		current_speed *= dash_speed
+		can_dash = false
+		
+		dash_timer.start()
+		
 	direction = direction.normalized()
-	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+	velocity = velocity.linear_interpolate(direction * current_speed, acceleration * delta)
 	velocity = move_and_slide(velocity, Vector3.UP)
 	move_and_slide(fall, Vector3.UP)
